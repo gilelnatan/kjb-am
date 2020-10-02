70a938381b7cc3149528b219896b2c99110b5b74
gilelnatan <72215102+gilelnatan@users.noreply.github.com>
2020-10-02 15:14:10 +0300

    Create jwplayer.js

---
+++ jwplayer.js
@@ -0,0 +1,9 @@
+//chapter 
+window._wq = window._wq || [];
+_wq.push({
+  id: "5bbw8l7kl5",
+  options: {
+    chaptersOn: "true",
+    chapterList: [{title: "Chapter 1", time: "5.0"}, {title: "Chapter 2", time: "20.0"}]
+  }
+});
